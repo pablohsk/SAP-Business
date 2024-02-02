310e6a90fae6bbb4bb5191d4cf5558029a574d4b
Pablohsk <pablofidelisdias@gmail.com>
2024-02-02 09:40:58 -0300

    Criacao do code urls.py

--- financeiro/apps.py
+++ financeiro/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
+
 class FinanceiroConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'erp_project.financeiro'
+    name = 'SAP_Business.financeiro'
