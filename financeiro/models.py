c4c6325b2b4031dbd007b5996965013cb5128c35
Pablohsk <pablofidelisdias@gmail.com>
2024-02-02 09:53:19 -0300

    Criacao do code views.py

--- financeiro/models.py
+++ financeiro/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
+
 class Transacao(models.Model):
     descricao = models.CharField(max_length=255)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     data = models.DateField()
 
+
 class Conta(models.Model):
     nome = models.CharField(max_length=255)
     saldo = models.DecimalField(max_digits=10, decimal_places=2)
     transacoes = models.ManyToManyField(Transacao)
+
 
 class Fatura(models.Model):
     numero = models.CharField(max_length=255)
