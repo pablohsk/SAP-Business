2e975b2a77125b3207196b12bfc8e187ad31827f
Pablohsk <pablofidelisdias@gmail.com>
2024-02-02 10:25:53 -0300

    Criacao do code urls.py

--- logistica/views.py
+++ logistica/views.py
@@ -1,12 +1,13 @@
-# logistica/views.py
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Transportadora, Pedido
+
 
 class TransportadoraListView(ListView):
     model = Transportadora
     template_name = 'logistica/transportadora_list.html'
     context_object_name = 'transportadoras'
+
 
 class PedidoListView(ListView):
     model = Pedido
